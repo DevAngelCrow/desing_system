9fd1a27e3fe02f2649006e0f836abfddcc57b597
angel_dev <angelguille1@hotmail.com>
2025-05-18 18:21:52 -0600

    05182025

--- storybook/App.tsx
+++ storybook/App.tsx
@@ -1,43 +1,12 @@
 
 import './App.css'
-import Button from "../src/components/buttons/Button";
-import InputText from "../src/components/inputs/InputText";
-import InputTextArea from "../src/components/inputs/InputTextArea";
-import InputCheckBox from "../src/components/inputs/InputCheckBox";
-import InputRadioButton from "../src/components/inputs/InputRadioButton";
-import InputSelect from "../src/components/inputs/InputSelect";
-import Modal from "../src/components/modal/Modal"
-import { useState } from 'react';
+import StoryBook from './Storybook'
+
 function App() {
-  const items = ["Prueba1", "Prueba2", "Prueba3"]
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const handleOpenModal = () => {
-    setIsModalOpen(true);
-  }
-  const handleCloseModal = () => {
-    setIsModalOpen(false);
-  }
+
   return (
     <>
-      <div className='flex flex-col'>
-        <Button className="bg-secondary text-primary" label="Botón" icon={{ name: "Alarm", position: "right" }}
-          onClick={() => console.log("onClick")} />
-        <br></br>
-        <InputText outline label='InputText' />
-        <br></br>
-        <InputTextArea label='InputTextArea' outline />
-        <br></br>
-        <InputCheckBox label='InputCheckBox' />
-        <br></br>
-        <div>
-          <InputRadioButton label="InputRadioButton" name="prueba" /><InputRadioButton label="InputRadioButton" name="prueba" />
-        </div>
-        <br></br>
-        <InputSelect items={items}/>
-        <br></br>
-        <Button onClick={handleOpenModal} className='bg-secondary text-primary' label='Modal' />
-        <Modal onClose={handleCloseModal} isOpen={isModalOpen} title="Modal" content={<div><span>Pruebas pruebas</span></div>}/>
-      </div>
+      <StoryBook />
     </>
   )
 }
---
+++ storybook/Storybook.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import Button from "../src/components/buttons/Button";
+import InputText from "../src/components/inputs/InputText";
+import InputTextArea from "../src/components/inputs/InputTextArea";
+import InputCheckBox from "../src/components/inputs/InputCheckBox";
+import InputRadioButton from "../src/components/inputs/InputRadioButton";
+import InputSelect from "../src/components/inputs/InputSelect";
+import Modal from "../src/components/modal/Modal"
+import { useState } from 'react';
+
+
+const StoryBook: React.FC = () => {
+    const items = ["Prueba1", "Prueba2", "Prueba3"]
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const handleOpenModal = () => {
+        setIsModalOpen(true);
+    }
+    const handleCloseModal = () => {
+        setIsModalOpen(false);
+    }
+    return (
+        <div className='flex flex-col'>
+            <Button className="bg-secondary text-primary" label="Botón" icon={{ name: "Alarm", position: "right" }}
+                onClick={() => console.log("onClick")} />
+            <br></br>
+            <InputText outline label='InputText' />
+            <br></br>
+            <InputTextArea label='InputTextArea' outline />
+            <br></br>
+            <InputCheckBox label='InputCheckBox' />
+            <br></br>
+            <div>
+                <InputRadioButton label="InputRadioButton" name="prueba" /><InputRadioButton label="InputRadioButton" name="prueba" />
+            </div>
+            <br></br>
+            <InputSelect items={items} />
+            <br></br>
+            <Button onClick={handleOpenModal} className='bg-secondary text-primary' label='Modal' />
+            <Modal onClose={handleCloseModal} isOpen={isModalOpen} title="Modal" content={<div><span>Pruebas pruebas</span></div>} />
+        </div>
+    );
+}
+
+export default StoryBook;
